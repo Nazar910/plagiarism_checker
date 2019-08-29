6b820f79e9c4f062f1cb4fdee4c688d05725b21c
Nazarii Pyvovar <pyvovarnazar@gmail.com>
2019-08-29 22:25:59 +0300

    clean up

--- 1.py
+++
@@ -1,9 +0,0 @@
-# Get the first 20 hits for: "Breaking Code" WordPress blog
-from googlesearch import search
-import urllib.request
-from inscriptis import get_text
-for url in search('"Breaking Code" WordPress blog', stop=1):
-    print(url)
-    html = urllib.request.urlopen(url).read().decode('utf-8')
-    print(html)
-    print(get_text(html))
